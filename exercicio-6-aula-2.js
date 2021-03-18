d279287ccea17f82dd66b65c7c408cad3817aca9
Paulo <pauloterresleal@gmail.com>
2021-03-18 16:17:10 -0300

    adicionando exercicios

---
+++ exercicio-6-aula-2.js
@@ -0,0 +1,118 @@
+// 6 - converter o exercicio 5 (de callback) para promise
+/*
+let promise = new Promise((resolve, reject)=> {
+  let resultado = false;
+  if (resultado) {
+    resolve("deu certo");
+  } else {
+    reject("deu errado");
+  }
+});
+
+promise.then((data) => console.log(`${data}`));
+promise.catch((data) => console.log(`${data}`));
+ 
+
+//não alterado
+  var list = [];
+
+const databaseInsert = function (data, callback) {
+  setTimeout(function () {
+    list.push(data);
+    if (callback) {
+      callback();
+    }
+  }, 10);
+};
+///////////////////////
+
+const parametro = {meuobjeto: "a"};
+const funcaoCallback = () => (console.log(list))
+
+databaseInsert(parametro, funcaoCallback); */
+
+
+
+
+const list = [];
+const databaseInsert = (data) => {
+  // console.log('data', data);
+
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      list.push(data);
+      // if (callback) {
+      //  callback();
+      // }
+      resolve(list);
+    }, 10);
+  })
+}
+
+// USANDO THEN
+/*
+  const promise = databaseInsert({meuobjeto: "a"});
+
+  // define o que será feito quando a promise for resolvida
+  promise.then((valorDoResolveDaPromise) => console.log('valorDoResolveDaPromise', valorDoResolveDaPromise));
+
+  // define o que será feito caso a promise seja rejeitada;
+  promise.catch((rejectDaPromise) => console.log('rejectDaPromise', rejectDaPromise))
+*/
+
+// USANDO ASYNC/AWAIT
+
+const funcaoAsync = async () => {
+  // o await faz com que aguardemos o resolve da promise 
+  const resolveDaPromise = await databaseInsert({meuobjeto: "a"});
+  console.log('resolveDaPromise', resolveDaPromise);
+}
+
+funcaoAsync();
+
+
+
+
+
+
+
+
+
+
+/*
+
+/************************** 
+//alterado
+var list = [];
+
+const databaseInsert = new dataBaseInsert (resolve, reject) { 
+  setTimeout (function () {
+   if(resolve){
+    resolve = list.push(data);
+    if (reject) {
+      reject();
+   }
+    }
+  }, 10);
+};
+///////////////////////
+databaseInsert({meuobjeto: "a"}, () => (console.log(list))); 
+
+promise
+.then((data) => console.log(`${data}`))
+.catch((data) => console.log(`${data}`));
+
+/************************** */
+
+/* //curso
+let promise = new Promise((resolve, reject)=> {
+  let resultado = false;
+  if (resultado) {
+    resolve("deu certo");
+  } else {
+    reject("deu errado");
+  }
+});
+
+promise.then((data) => console.log(`${data}`));
+promise.catch((data) => console.log(`${data}`));  */
---
+++ exercicio-7-aula-2.js
@@ -0,0 +1,2 @@
+// 7 - converter o exercicio 6 para async await
+
